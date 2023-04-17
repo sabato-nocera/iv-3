0097ce93d0575f0fc54c77074adf1dfa0f54e273
sabato-nocera <nocera.sabato@gmail.com>
2023-04-17 16:28:34 +0200

    Initial commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add() {
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void subtract() {
+        int result = calculator.subtract(2, 3);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void multiply() {
+        int result = calculator.multiply(2, 3);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void divide() {
+        int result = calculator.divide(3, 3);
+        assertEquals(1, result);
+    }
+}
